a97463e19adf63900ff8c0465e14f2ba1e9b57f7
DymaxionMap <DymaxionMap@users.noreply.github.com>
2015-03-14 21:43:46 -0500

    Parse POST data

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function(request, response) {
   response.render('index');
+});
+
+app.post('/', function(request, response) {
+  var info = request.body;
+  console.log("Sex: " + info.sex + ", Age: " + info.age);
+  response.sendStatus(200);
 });
 
 app.listen(app.get('port'), function() {
