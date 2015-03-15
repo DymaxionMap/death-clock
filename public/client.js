0ca754f2f74f2716da5b2a0603d41221ab33d783
DymaxionMap <DymaxionMap@users.noreply.github.com>
2015-03-15 17:38:04 -0500

    Convert life expectancy to Death Date

--- public/client.js
+++ public/client.js
@@ -1,14 +1,25 @@
 $(document).ready(function() {
-  $(".user-info").submit(function(event) {
+
+  $(".user-form").submit(function(event) {
     event.preventDefault();
     $.post("/", $(this).serialize())
       .done(function(user) {
-        console.log(user);
         user = JSON.parse(user);
         console.log("Life expectancy: " + user.life_expectancy);
+        var life_expectancy = user.life_expectancy;
+        $(".clock").text(getDeathDate(life_expectancy));
       })
       .fail(function() {
         console.log("There was an error...");
       });
   });
+
+  function getDeathDate(life_expectancy) {
+    var deathDate = new Date();
+    console.log("Now: " + deathDate);
+    var deathYear = deathDate.getFullYear() + Math.round(life_expectancy);
+    deathDate.setFullYear(deathYear);
+    console.log("Death Date: " + deathDate);
+    return deathDate;
+  };
 });
